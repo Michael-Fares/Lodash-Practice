fd827e2b48495d581cd05dee6379978b9e2f353f
Michael-Fares <mjfares@gmail.com>
2021-06-17 09:59:01 -0500

    wrote tests for lodash methods

--- main.js
+++ main.js
@@ -40,6 +40,8 @@ let fakePeople = [
 
 let myArray = [1,14,44,30,2,4,76,10]
 
+let myNewArray = [1,2,3,4]
+
 console.log('chunk',_.chunk(myArray, [size=3]))
 console.log('chunk',_.chunk(fakePeople, [size=4]))
 
@@ -53,6 +55,43 @@ console.log('shuffle',_.shuffle(fakePeople))
 console.log('nth',_.nth(myArray, [n=5]))
 console.log('nth',_.nth(fakePeople, [n=2]))
 
-console.log('reverse',_.reverse(myArray))
+console.log('reverse',_.reverse(myNewArray))
 console.log('reverse',_.reverse(fakePeople))
 
+// unit tests
+const assert = require('assert');
+
+if (typeof describe === 'function') {
+
+  describe('#_.chunk()', function() {
+    it('should break an array into equal sizes of the size specified, and nest those in one array', function()  {
+      assert.deepEqual(_.chunk([1,2,3,4], 2), 
+      [[1,2],[3,4]]);
+    });
+  });
+  describe('#_.reverse()', function() {
+    it('should reverse an array', function()  {
+      assert.deepEqual(_.reverse([1,2,3,4]), 
+      [4,3,2,1]);
+    });
+  });
+  describe('#_.without()', function() {
+    it('remove the specified elements from the array', function()  {
+      assert.deepEqual(_.without([1,2,3,4], 4), 
+      [1,2,3]);
+    });
+  });
+  describe('#_.nth()', function() {
+    it('should give the nth element in the array', function()  {
+      assert.deepEqual(_.nth([1,2,3,4], 1), 
+      2);
+    });
+  });
+
+  // is there really a way to write a reliable test for the shuffle method other than making sure the output doesnt match the input?
+  describe('#_.shuffle()', function() {
+    it('shuffle an array', function()  {
+      assert.notDeepEqual(_.shuffle([1,2,3,4]), [1,2,3,4]);
+    });
+  });
+}
